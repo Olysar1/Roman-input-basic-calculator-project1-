eeeb04be446d380e203de70534eb33ccc5844298
Olysar1 <giorgibarbakadz1@gmail.com>
2022-10-30 02:21:15 +0400

    add roman to arabic converter

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
     //////////////////////////////try4///////////////////////////////////
 
+
+    //converter map:
+    static Map<Character, Integer> romanToArabicMap = new HashMap<>(){{
+        put('I',1);
+        put('V',5);
+        put('X',10);
+        put('L',50);
+        put('C',100);
+        put('D',500);
+        put('M',1000);
+    }};
+
+    //converter function(Arabic to Roman);
+
+    //converter function(Roman to Arabic):
+    public static int romanToArabic(String roman) {
+
+        int arabicNum = romanToArabicMap.get(roman.toUpperCase().charAt(roman.length()-1));
+
+        for (int i=roman.length()-2; i>=0; i--) {
+            if(romanToArabicMap.get(roman.toUpperCase().charAt(i)) >= romanToArabicMap.get(roman.toUpperCase().charAt(i+1))) {
+                arabicNum += romanToArabicMap.get(roman.toUpperCase().charAt(i));
+            } else if ((romanToArabicMap.get(roman.toUpperCase().charAt(i)) < romanToArabicMap.get(roman.toUpperCase().charAt(i+1)))) {
+                arabicNum -= romanToArabicMap.get(roman.toUpperCase().charAt(i));
+            }
+        }
+        return arabicNum;
+    }
+
     //operator functions:
     public static int result = 0; //keep the latest result
+    static Scanner scan = new Scanner(System.in); //scanner
 
     public static void add(int x, int y) {
         result = x + y;
@@ -32,28 +62,32 @@ public class Main {
         System.out.println("The current result is: "+result);
     }
 
-
+//////////////////////////////////////
     public static void main(String[] args) {
+
+//        int p =  romanToArabic("McMXlVIII");
+//        System.out.println(p);
 
         //variable area:
         int num1, num2;
         String operator;
-        Scanner scan = new Scanner(System.in);
 
         //calculator code:
         System.out.println("enter the first number: ");
-        num1 = scan.nextInt();
+        num1 = romanToArabic(scan.nextLine());
         result = num1;
 
         while(true) {
             System.out.println("enter operator(or \"end\" to terminate the calculator): ");
-            operator = scan.next();
+            operator = scan.nextLine();
             if (operator.equals("end")) {
                 System.out.println("calculator terminated. final result: "+result);
                 break;
             }
             System.out.println("enter a number: ");
-            num2 = scan.nextInt();
+            String x = scan.nextLine();
+            num2 = romanToArabic(x);
+
             switch(operator) {
                 case "+":
                     add(result, num2);
